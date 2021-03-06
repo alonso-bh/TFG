8663dcb3fccc29b388914d12fb53a41a762332eb
alonso-bh <alonsobueno13@correo.ugr.es>
2021-03-06 02:02:05 +0100

    datos hasta el día 5/3

--- descargar_datasets.R
+++ descargar_datasets.R
@@ -1,9 +1,30 @@
+install.packages("RCurl")
+library(RCurl)
 
 
-dir.create('04-04')
+# función para concatenar
+concatenar_strings <- function(cad1, cad2){
+  tmp <- cbind(cad1, cad2)
+  library(stringr)
+  str_c(tmp, collapse = "")
+}
+
+# preparar el formato de fecha para nombrar así a la carpeta de hoy
+hoy <- format(Sys.time(), "%d-%m") # formato establecido: dd-mm
+
+# crear la carpeta para el día de hoy
+
+
+setwd("datos")
+nombre_carpeta <- concatenar_strings("\\datos\\", hoy)
+nombre_carpeta
+dir.create(nombre_carpeta)
+setwd(hoy)
 
 # descargar ficheros de datos por provincia-dist.sanit.-pueblo
-url_1 <- ""
+# almería
+download.file("https://www.juntadeandalucia.es/institutodeestadisticaycartografia/salud/static/xls/Covid_04.xls",destfile="Covid_04.xls",method="libcurl")
 
 # descargar datos de hospitalizaciones por provincias
-url_hosp <- ""
+
+
